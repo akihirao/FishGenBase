b73a9bf2f2f4e8304a3d49ba7fb9a61f3e197987
akihirao <akihirao@gmail.com>
2023-04-24 05:41:08 +0900

    update the scraping code & list

--- chronology/plot.no_genome_deposited.R
+++ chronology/plot.no_genome_deposited.R
@@ -8,6 +8,8 @@ list20220915 <- read_csv("aquatic_organism_genome_size_2022_0915.csv")
 list20230221 <- read_csv("aquatic_organism_genome_size_2023_0221.csv")
 list20230313 <- read_csv("aquatic_organism_genome_size_2023_0313.csv")
 list20230329 <- read_csv("aquatic_organism_genome_size_2023_0329.csv")
+list20230421 <- read_csv("aquatic_organism_genome_size_2023_0421.csv")
+
 
 No_sp_genome_20100331 <- 1
 No_sp_genome_20200421 <- 18
@@ -15,6 +17,8 @@ No_sp_genome_20220915 <- sum(!is.na(list20220915$Genome_size_of_the_species_Mbp)
 No_sp_genome_20230221 <- sum(!is.na(list20230221$Genome_size_of_the_species_Mbp)) 
 No_sp_genome_20230313 <- sum(!is.na(list20230313$Genome_size_of_the_species_Mbp)) 
 No_sp_genome_20230329 <- sum(!is.na(list20230329$Genome_size_of_the_species_Mbp)) 
+No_sp_genome_20230421 <- sum(!is.na(list20230421$Genome_size_of_the_species_Mbp)) 
+
 
 No_sp_genus_20100331 <- 3
 No_sp_genus_20200421 <- 43
@@ -22,15 +26,19 @@ No_genus_genome_20220915 <- sum(!is.na(list20220915$Average_genome_size_of_the_g
 No_genus_genome_20230221 <- sum(!is.na(list20230221$Average_genome_size_of_the_genus_Mbp)) 
 No_genus_genome_20230313 <- sum(!is.na(list20230313$Average_genome_size_of_the_genus_Mbp)) 
 No_genus_genome_20230329 <- sum(!is.na(list20230329$Average_genome_size_of_the_genus_Mbp)) 
+No_genus_genome_20230421 <- sum(!is.na(list20230421$Average_genome_size_of_the_genus_Mbp)) 
 
 
 genome_chronology <- tibble(
   date = c(ymd("2010-03-31"),ymd("2020-04-21"),ymd("2022-09-15"), 
-           ymd("2023-02-21"),ymd("2023-03-13"),ymd("2023-03-29")), 
+           ymd("2023-02-21"),ymd("2023-03-13"),ymd("2023-03-29"),
+           ymd("2023-04-21")), 
   Species = c(No_sp_genome_20100331, No_sp_genome_20200421, No_sp_genome_20220915,
-              No_sp_genome_20230221,No_sp_genome_20230313,No_sp_genome_20230329),
+              No_sp_genome_20230221,No_sp_genome_20230313,No_sp_genome_20230329,
+              No_sp_genome_20230421),
   Genus = c(No_sp_genus_20100331,No_sp_genus_20200421,No_genus_genome_20220915,
-            No_genus_genome_20230221,No_genus_genome_20230313,No_genus_genome_20230329)
+            No_genus_genome_20230221,No_genus_genome_20230313,No_genus_genome_20230329,
+            No_genus_genome_20230421)
             )
 
 
